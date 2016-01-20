325aa627c80729294605b411e1690224d342e86d
KamilZet <kamilxzien@gmail.com>
2016-01-20 17:16:04 +0100

    Folders added

---
+++ HELLO_NHIBERNATE/Domain/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HELLO_NHIBERNATE.Domain
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public bool Discontinued { get; set; }
+    }
+}
